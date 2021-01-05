474f55d3eb2dbca171a279a7d3e6f680adb5ce89
iamgique <iamgique@gmail.com>
2021-01-05 18:31:12 +0700

    first commit

---
+++ src/com/iamgique/Solution.java
@@ -0,0 +1,42 @@
+package com.iamgique;
+
+public class Solution {
+    public static void main(String[] args) {
+        int input = 9;
+        oddDiamond(input);
+    }
+
+    public static void oddDiamond(int input){
+        // ----X----
+        // ---X-X---
+        // --X---X--
+        // -X-----X-
+        // X-------X
+        // -X-----X-
+        // --X---X--
+        // ---X-X---
+        // ----X----
+
+        for(int i = 0; i < input; i++) {
+            for(int j = 0; j < input; j++) {
+
+                if(i <= input / 2) {
+                    if(j == input / 2 - i || input - j == input / 2 - i + 1) {
+                        System.out.print("X");
+                    } else {
+                        System.out.print("-");
+                    }
+                }
+                if(i > input / 2) {
+                    if(j == i - input / 2 || input - j == i + 1 - input / 2) {
+                        System.out.print("X");
+                    } else {
+                        System.out.print("-");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/com/iamgique/readme.md
@@ -0,0 +1,21 @@
+# Diamond
+This project is doing for easy algorithm.
+
+
+## Problem
+#### Odd diamond
+Write program get input `n` (only odd number)
+
+For example input n = 9 then 
+
+```
+----X---- 
+---X-X---
+--X---X--
+-X-----X-
+X-------X
+-X-----X-
+--X---X--
+---X-X---
+----X----
+```
